9ca1ea53378c968c99d36208d066d71af468420e
praveenterax <praveendunga111@gmail.com>
2024-05-22 19:01:47 +0530

    fixed build issue

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -27,7 +27,6 @@ export const ServerChannel = ({
 
   const params = useParams();
   const router = useRouter();
-  const { channelId } = params;
   const Icon = iconMap[channel.type];
 
   const onChannelClick = () => {
@@ -45,14 +44,14 @@ export const ServerChannel = ({
         onClick={onChannelClick}
         className={cn(
           'group px-2 py-2 flex items-center gap-x-2 rounded-md w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
-          channel?.id === channelId && 'bg-zinc-700/20 dark:bg-zinc-700'
+          channel?.id === params?.channelId && 'bg-zinc-700/20 dark:bg-zinc-700'
         )}
       >
         <Icon className="flex-shrink-0 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
         <p
           className={cn(
             'line-clamp-1 font-semibold text-sm text-zinc-500 dark:text-zinc-400/80 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition w-full text-left',
-            channel?.id === channelId &&
+            channel?.id === params?.channelId &&
               'text-primary dark:text-zinc-200 dark:group-hover:text-white'
           )}
         >
